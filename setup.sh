1ee7855ad7c6afaac80ecd4efd7e5cfc7f30f5b9
Glenn Chugg <gchugg@outlook.com>
2025-03-02 20:43:30 +1100

    Added gnome fix to setup.sh so the very first terminal is shown correctly

--- setup.sh
+++ setup.sh
@@ -35,6 +35,18 @@ else
     echo "error can't install package $PACKAGES"
 fi
 
+#Fix gnome terminal theme on Wayland
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
+    profile=${profile:1:-1} # remove leading and trailing single quotes
+
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ use-theme-colors 'false'
+
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ foreground-color 'rgb(208,207,204)'
+
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ background-color 'rgb(23,20,33)'
+fi  #Wayland check
+
 fi  #Top gnome check
 
 
